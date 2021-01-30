0e6de0bd9722bde6a6a2f1847da87ba3e7c109d2
Rewieshschool <re.ramcharan@unasat.sr>
2021-01-30 12:54:06 -0300

    created user table and dao class

---
+++ app/src/main/java/unasat/sr/buysmart/DAO/DAO.java
@@ -0,0 +1,40 @@
+package unasat.sr.buysmart.DAO;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class DAO extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME = "buysmart.db";
+    public static int DATABASE_VERSION = 2;
+
+    public DAO(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DAO(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    public DAO(@Nullable Context context, @Nullable String name, int version, @NonNull SQLiteDatabase.OpenParams openParams) {
+        super(context, name, version, openParams);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
