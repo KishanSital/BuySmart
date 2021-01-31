39d8e03d20b31fd377cbbab336e4554dae90af81
KishanSital <kishansital1@gmail.com>
2021-01-31 17:23:14 -0300

    REST COUNTRY

---
+++ app/src/main/java/unasat/sr/buysmart/Entities/Country.java
@@ -0,0 +1,22 @@
+package unasat.sr.buysmart.Entities;
+
+public class Country {
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
